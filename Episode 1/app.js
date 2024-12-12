e2fe7998bcb089db85c768ca32f915d5b483cf4f
Pratiksha Kulkarni <pratiksha.kulkarni@lexiconnetworks.com>
2024-12-12 17:19:28 +0530

    Episide 1 of namaste react

---
+++ Episode 1/app.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Welcome to 1st react project"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
